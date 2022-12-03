389d0e5f946e1aac094caa181160a0c33cda420e
pratimes <108912069+pratimes@users.noreply.github.com>
2022-12-03 14:23:27 +0530

    Create monitor_speedtest.py

---
+++ monitor_speedtest.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+import datetime
+import speedtest
+from influxdb import InfluxDBClient
+
+# influx configuration - edit these
+ifuser = "grafana"
+ifpass = "grafana"
+ifdb   = "home"
+ifhost = "127.0.0.1"
+ifport = 8086
+measurement_name_speedtest = "speedtest"
+
+# take a timestamp for this measurement
+time = datetime.datetime.utcnow()
+
+# run a single-threaded speedtest using default server
+s = speedtest.Speedtest()
+s.get_best_server()
+s.download(threads=1)
+s.upload(threads=1)
+res = s.results.dict()
+
+# format the data as a single measurement for influx
+body_speedtest = [
+    {
+        "measurement": measurement_name_speedtest,
+        "time": time,
+        "fields": {
+            "download": res["download"],
+            "upload": res["upload"],
+            "ping": res["ping"]
+        }
+    }
+]
+
+
+# connect to influx
+ifclient = InfluxDBClient(ifhost,ifport,ifuser,ifpass,ifdb)
+
+# write the measurement
+ifclient.write_points(body_speedtest)
